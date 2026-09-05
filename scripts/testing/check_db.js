a00aa1539f4d554a1d154a0b5aca76ef7e821467
Kav-Senpai <106959520+Kav-Senpai@users.noreply.github.com>
2026-09-05 15:41:40 +0530

    Person 1 and 2 merge complete

--- scripts/testing/check_db.js
+++ scripts/testing/check_db.js
@@ -1,4 +1,4 @@
-// Temporary bridge script until Supabase->analysis wiring is decided\nconst SUPABASE_URL = "https://bnnrikqlhvcqpuqsujsf.supabase.co/rest/v1";
+const SUPABASE_URL = "https://bnnrikqlhvcqpuqsujsf.supabase.co/rest/v1";
 const KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJubnJpa3FsaHZjcXB1cXN1anNmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4ODU4Nzg5MCwiZXhwIjoyMTA0MTYzODkwfQ.3ffdNWfb2g0GmC1UqEE_Vma-nLsgXSjXkWJPAqPAM7M";
 
 async function run() {
